482d858d44220b599eff5411e83ee5f6faf61fae
Ainsley <ainsleyjyl@gmail.com>
2024-02-29 17:23:38 -0800

    add about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,7 +10,28 @@ export default function About() {
             animate={{ y: '0%' }}
             transition={{ duration: 1 }}
         >
-            About
+            {/* Container */}
+            <div>
+                {/* text container */}
+                <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64'>
+                    {/* biography */}
+                    <div className='flex flex-col gap-12 justify-center'>
+                        <h1>BIOGRAPHY</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum harum
+                            quibusdam cupiditate nobis accusamus sed aut aperiam, reiciendis
+                            numquam! Voluptas voluptatibus obcaecati dolore itaque suscipit! Vel
+                            doloremque numquam quam nihil.
+                        </p>
+                        <span></span>
+                    </div>
+                    {/* skills */}
+                    <div>skills</div>
+                    {/* experiences */}
+                    <div>experiences</div>
+                </div>
+                {/* svg container */}
+            </div>
         </motion.div>
     );
 }
