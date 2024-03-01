aca1c64cc15a484f189added090f1783ed51445a
Ainsley <ainsleyjyl@gmail.com>
2024-03-01 11:43:38 -0800

    add svg & animation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import Brain from '@/components/Brain';
+import { motion, useScroll } from 'framer-motion';
+import { useRef } from 'react';
 
 export default function About() {
+    const containerRef = useRef();
+    const { scrollYProgress } = useScroll({ container: containerRef });
     return (
         <motion.div
             className='h-full'
@@ -11,7 +15,10 @@ export default function About() {
             transition={{ duration: 1 }}
         >
             {/* Container */}
-            <div className='h-full overflow-scroll lg:flex'>
+            <div
+                className='h-full overflow-scroll lg:flex'
+                ref={containerRef}
+            >
                 {/* text container */}
                 <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2'>
                     {/* biography */}
@@ -206,7 +213,9 @@ export default function About() {
                     </div>
                 </div>
                 {/* svg container */}
-                <div className='hidden lg:block w-1/3 xl:w-1/2'></div>
+                <div className='hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2'>
+                    <Brain scrollYProgress={scrollYProgress} />
+                </div>
             </div>
         </motion.div>
     );
